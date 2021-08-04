82422b8a62f21724ba4b3f4cb9a2b336f51e3942
Miguel <miguelarrieta02@gmail.com>
2021-08-04 19:25:57 +0100

    Implemented function to request API data

--- check_my_pass.py
+++ check_my_pass.py
@@ -1,5 +1,22 @@
+import requests
+import hashlib
+
+
+def request_api_data(query_chars: str) -> requests.Response:
+    response = requests.get(f'https://api.pwnedpasswords.com/range/{query_chars}')
+    if response.status_code != 200:
+        raise RuntimeError(f'Error fetching: {response.status_code}, check the API and try again')
+    return response
+
+
+def pwned_api_check(password: str):
+    sha1_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1_password
+
+
 def main():
-    pass
+    password = 'password123'
+    request_api_data('12345')
 
 
 if __name__ == '__main__':
